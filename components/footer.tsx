96892a6491188e670e06b98829f8aba5e2405151
Jon Jackson <jon@acuras.co.uk>
2026-08-28 17:46:19 +0100

    tweak: Update footer to include links for GitHub Copilot and Mistral AI

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
       <p className="text-sm m-4">
         Built by <a className="!text-blue-500 !hover:text-blue-700 " href="https://github.com/iamjonjackson" target="_blank" rel="noopener noreferrer">
           iamjonjackson
-        </a> and GitHub Copilot as part of the <a className="!text-blue-500 !hover:text-blue-700 " href="https://www.qmul.ac.uk/queenmaryacademy/educators/innovation-and-scholarship/innovative-pedagogies/centre-for-excellence-in-ai-in-education/" target="_blank" rel="noopener noreferrer">
+        </a> with <a className="!text-blue-500 !hover:text-blue-700 " href="https://github.com/features/copilot" target="_blank" rel="noopener noreferrer">GitHub Copilot</a> + <a className="!text-blue-500 !hover:text-blue-700 " href="https://mistral.ai" target="_blank" rel="noopener noreferrer">Mistral AI</a> as part of the <a className="!text-blue-500 !hover:text-blue-700 " href="https://www.qmul.ac.uk/queenmaryacademy/educators/innovation-and-scholarship/innovative-pedagogies/centre-for-excellence-in-ai-in-education/" target="_blank" rel="noopener noreferrer">
           QMUL Centre for Excellence in AI in Education
         </a> Programme Redesign Accelerator Project.
       </p>
